0be7bd788ba036a7e6d3182c052327b877db1b6d
Georgi Georgiev <vuryss@gmail.com>
2019-09-02 13:31:01 +0300

    Added phpstan to the code quality testing tools

--- tests.sh
+++ tests.sh
@@ -4,3 +4,4 @@
 php /home/user/phpcs.phar -s
 php /home/user/phpmd.phar --exclude vendor,tests . text codesize,unusedcode,naming,controversial,cleancode,design
 php /home/user/phan.phar
+php /home/user/phpstan.phar analyse src --level max
